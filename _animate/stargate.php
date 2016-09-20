971e7f3b3c67696c79751b8d4d877eafef3dc526
webdesigngse <webstagiair@GSEAutomatisering.local>
2016-09-20 16:52:27 +0200

    Revert "BC0001"
    This reverts commit 5f68117c21966d5ad3b5be543d45ab2b5d4c573b.

--- _animate/stargate.php
+++
@@ -1,4 +0,0 @@
-<video autoplay loop poster="_images/citadel_80_burn.jpg" id="bgvid">
-    <source src="/_animate/gate.webm" type="video/webm">
-    <source src="/_animate/gate.mp4" type="video/mp4">
-</video>
