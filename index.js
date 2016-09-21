6a099cad912997844e242f5e323d55ee37b10da2
webdesigngse <webdesign@gse.nl>
2016-09-21 14:07:03 +0200

    C00:0002
    Pogingen tot proper navigatie worden gezet.

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-var express = require('express'),
-    app = express.createServer();
+var express = require('express');
+var app     = express();
+var http    = require('http').Server(app);
+var io      = require('socket.io')(http);
+var $       = require('jquery');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/hello', function(req, res){
-  res.send('hello world');
+/* luister naar local port; 3000 */
+http.listen(3000, function(){
+  console.log('Activity detected on pathway *:3000 . Stand by for engagement.')
 });
 
-//The 404 Route (ALWAYS Keep this as the last route)
-app.get('*', function(req, res){
-  res.send('what???', 404);
+
+
+app.get('/', function(req, res){
+  console.log('meme');
+  res.send('index.html');
 });
 
-app.listen(3000, '127.0.0.1');
+app.get('/loaded', function(req, res){
+  console.log('GUBAT NAVIGATIE SUCCESVOL GEPORT, PRAISE THE METAL MOT');
+  res.send('loaded.html');
+});
+
+io.on('connection', function (socket) {
+
+  console.log('User [  ] has connected.');
+
+  socket.on('disconnect', function(){
+    console.log('user [  ] signed out.');
+  });
+
+  socket.on('loadedpage', function(msg){
+    io.emit('loadedpage', msg);
+  });
+
+});
